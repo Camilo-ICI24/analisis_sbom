81cd4b7b701f797b38ed5a4507939b49bdab61c7
camilo_ici <c.cifuentes14@ufromail.cl>
2026-04-22 22:39:33 -0400

    Adición de lógica para clonar repositorios desde archivo repos.txt

--- escaneo/scan.sh
+++ escaneo/scan.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
-echo "🚀 Iniciando escaneo SBOM + vulnerabilidades..."
+echo "Iniciando proceso de clonación y análisis!!!..."
 
 start=$(date +%s)
 
+mkdir -p repos
 mkdir -p datos/sboms
 mkdir -p datos/vulns
+
+while read repo_url; do
+  name=$(basename "$repo_url" .git)
+
+  if [ ! -d "repos/$name" ]; then
+    echo "Clonando $name, espere...."
+    git clone "$repo_url" "repos/$name"
+  else
+    echo "✔ $name ya existe, continuando con el próximo...."
+  fi
+done < repos.txt
 
 for repo in repos/*; do
   name=$(basename "$repo")
 
-  echo "📦 Procesando $name..."
+  echo "Analizando y procesando $name..."
 
   syft "$repo" -o json > "datos/sboms/$name.json"
 
   grype "sbom:datos/sboms/$name.json" -o json > "datos/vulns/$name.json"
-
 done
 
 end=$(date +%s)
 
-echo "✅ Escaneo terminado"
+echo "✅ Proceso terminado"
 echo "⏱ Tiempo total: $((end - start)) segundos"
